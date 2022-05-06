3ec89af774d36b7d1880b121e49d448ea52133fc
Binh Ho <abinh.ho@gmail.com>
2022-05-06 16:32:47 +0700

    add redirect page

---
+++ src/pages/Redirect.vue
@@ -0,0 +1,27 @@
+<template>
+  <Layout>
+    <section></section>
+  </Layout>
+</template>
+
+<script>
+import homeData from '~/data/home.yml'
+
+export default {
+  components: {},
+  metaInfo() {
+    return {
+      title: homeData.title[this.locale],
+      meta: [{ name: 'description', content: homeData.description[this.locale] }],
+    }
+  },
+  data() {
+    return {
+      locale: this.$i18n.locale,
+    }
+  },
+  mounted() {
+    location.href = `https://apps.apple.com/vn/app/tapnow-widget-for-friends/id1612005183`
+  },
+}
+</script>
